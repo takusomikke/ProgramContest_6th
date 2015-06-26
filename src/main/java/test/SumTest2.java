40df44adc35211dd7ed4a6d81ecd7618edab1ece
takumi <takumi@takumi-PC>
2015-06-27 07:17:28 +0900

    HashMapを使ってforループを一つ減らした見つけ方。

---
+++ src/main/java/test/SumTest2.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SumTest2 {
+    public static void main(String[] args) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        //3本の合計値が入ります。
+        int sumlength= sc.nextInt();
+
+        //入力される本数が入ります。
+        int number = sc.nextInt();
+
+        //numberの長さを持つ配列を作ります。
+        int[] lengtharray = new int[number];
+
+        //3つ目の数字を見つけるためのマップです。
+        Map<Integer,Integer> lengthmap = new HashMap<Integer,Integer>();
+
+        for(int i = 0; i < number; i++){
+            int length = sc.nextInt();
+            lengtharray[i]=length;
+            lengthmap.put(sumlength-length, length);
+        }
+
+        int count=0;
+
+        for(int i = 0 ; i < number;i++){
+            for(int j = 0; j< number ; j++){
+
+                if(((lengtharray[i]*2+lengtharray[j] !=sumlength)||(lengtharray[i]+lengtharray[j]*2 !=sumlength))&&(lengthmap.get(lengtharray[i]+lengtharray[j]) != null)){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("組み合わせの数"+count/6);
+
+    }
+}
