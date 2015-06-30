a664d77c09dc1dd8f0a0a32b7104fa53dd203a7f
levelfive <levelfive@Levelfive-PC>
2015-06-30 09:32:51 +0900

    &&とすべきところを||としていたところを修正。 計算に無駄があった部分を修正。

--- src/main/java/test/SumTest2.java
+++ src/main/java/test/SumTest2.java
@@ -8,7 +8,15 @@ public class SumTest2 {
     public static void main(String[] args) {
 
 //        final int NUMBER_OF_CHOICES=3;
-        final int NUMBER_OF_COMBINATION = 6;
+
+        /*
+         * num1,num2,num3(昇順とする)の組み合わせを考える。
+         * (①,②,③)に当てはめるとすると、
+         * 今回は ① < ② の制約がある。この時のすべての組み合わせは、
+         * (num1,num2,num3),(num1,num3,num2),(num2,num3,num1)
+         * この3とおりである。
+         */
+        final int NUMBER_OF_COMBINATION = 3;
 
 
         @SuppressWarnings("resource")
@@ -26,6 +34,13 @@ public class SumTest2 {
         //3つ目の数字を見つけるためのマップです。
         Map<Integer,Integer> lengthmap = new HashMap<Integer,Integer>();
 
+        /* 1本ずつの長さの入力を受け付けます。
+         *
+         * キーの値は3つ目の数字として呼び出されるときの値を入れています。
+         * 例)入力された数字が2の場合
+         * 1本目、2本目の値の合計が8の時に3本目として2が必要とされる。
+         * よって、キーを8、値を2としている。
+         */
         for(int i = 0; i < number; i++){
             int length = sc.nextInt();
             lengtharray[i]=length;
@@ -35,10 +50,10 @@ public class SumTest2 {
         int count=0;
 
         for(int i = 0 ; i < number;i++){
-            for(int j = 0; j< number ; j++){
+            for(int j = i+1; j< number ; j++){
 
                 //2+6+2=10のような組み合わせではなく、かつ3文字目にフィットする文字が見つかった時にカウントする。
-                if(((lengtharray[i]*2+lengtharray[j] !=sumlength)||(lengtharray[i]+lengtharray[j]*2 !=sumlength))&&(lengthmap.get(lengtharray[i]+lengtharray[j]) != null)){
+                if((lengtharray[i]*2+lengtharray[j] !=sumlength)&&(lengtharray[i]+lengtharray[j]*2 !=sumlength)&&(lengthmap.get(lengtharray[i]+lengtharray[j]) != null)){
                     count++;
                 }
             }
